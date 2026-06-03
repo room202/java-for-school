12a5c7333d8f0b037f11d12242883220fc57cadf
room202 <yagi@i-seifu.jp>
2026-06-03 10:12:11 +0900

    Printfの練習ソースコードを追加

---
+++ 0603/Printf.java
@@ -0,0 +1,15 @@
+public class Printf {
+    public static void main(String[] args){
+        // C言語
+        // #include<stdio.h>
+        // int main() {
+        //     int value = 100;
+        //     printf("%d", value);
+        //     return 0;
+        // }
+        int value = 100;
+        double value2 = 3.14;
+        System.out.printf("%d  %f\n", value, value2);
+        System.out.println(value + " " + value2);
+    }
+}
